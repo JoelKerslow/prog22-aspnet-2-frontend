666ff21acfea952c8250f39da9eb33795d0d3be5
Moonlit <100379902+MrMoonlit@users.noreply.github.com>
2023-04-28 11:08:55 +0200

    Create riktlinjer.js

---
+++ src/scripts/riktlinjer.js
@@ -0,0 +1,10 @@
+// Dangling commas illustreras nedan
+
+// const danglingcommas = {
+//     'marcus' : 'korv',
+//     'joel': 'ocksåkorv', <= detta kommatecknet i slutet av sista raden
+// }
+
+// const arrowFunction = () => {
+//     Vi skriver funktioner på det här viset
+// }
