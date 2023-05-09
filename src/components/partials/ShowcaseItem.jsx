37780721dead6b9ca61ceaba1013b93e1acd95cf
MrMoonlit <100379902+MrMoonlit@users.noreply.github.com>
2023-05-09 15:51:23 +0200

    platformCommit

--- src/components/partials/ShowcaseItem.jsx
+++ src/components/partials/ShowcaseItem.jsx
@@ -3,10 +3,10 @@ import ShowcaseImage from "../../assets/images/showcase.png";
 
 const ShowcaseItem = ({ item }) => {
   return (
-      <div className="carousel-item">
+      <div className="showcase-item">
         <div className="content">
-          <div className="title-1">{item.title}</div>
-          <div className="title-1">{item.offer}</div>
+          <div className="title-text">{item.title}</div>
+          <div className="offer-text">{item.offer}</div>
         </div>
         <img className="showcase-img" src={ShowcaseImage} alt="A very sexy man" />
       </div>
