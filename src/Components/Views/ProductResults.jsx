4f084edd309a97d8313ec38e2eaf32621a810d86
Werre6 <oscarwerre@gmail.com>
2023-05-10 18:26:02 +0200

    message classnames

--- src/Components/Views/ProductResults.jsx
+++ src/Components/Views/ProductResults.jsx
@@ -28,11 +28,11 @@ const ProductResults = () => {
     <>
       <Header headerContent={<SearchField />} />
       {loading ? (
-        <div>Loading products...</div>
+        <div className='product-loading-message'>Loading products...</div>
       ) : (
         <>
           <div className="product-result-list">{productList}</div>
-          {productList.length === 0 && <h1>No products found</h1>}
+          {productList.length === 0 && <div className='no-products-message'>No products found</div>}
         </>
       )}
     </>
