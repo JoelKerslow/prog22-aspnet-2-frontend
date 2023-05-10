ac4783b7498c0e1ee47f251857d61e9bd8d4a9b9
Moonlit <100379902+MrMoonlit@users.noreply.github.com>
2023-05-10 15:23:54 +0200

    afternoon

--- src/components/partials/Showcase.jsx
+++ src/components/partials/Showcase.jsx
@@ -1,66 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import ShowcaseItem from './ShowcaseItem';
-import { ShowcaseAsync } from '../../Services/ShowcaseServices';
+import React, { useState, useEffect } from "react";
+import ShowcaseItem from "./ShowcaseItem";
+import { ShowcaseAsync } from "../../Services/ShowcaseServices";
 
 const Showcase = () => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const [showcase, setShowcase] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [showcase, setShowcase] = useState([]);
 
-	useEffect(() => {
-		const fetchShowcaseAsync = async () => {
-			ShowcaseAsync().then((data) => {
-				setShowcase(data);
-			});
-		};
-		fetchShowcaseAsync();
-	}, []);
+  useEffect(() => {
+    const fetchShowcaseAsync = async () => {
+      ShowcaseAsync().then((data) => {
+        setShowcase(data);
+      });
+    };
+    fetchShowcaseAsync();
+  }, []);
 
-	const updateIndex = (newIndex) => {
-		if (newIndex < 0) {
-			newIndex = 0;
-		} else if (newIndex >= showcase.length) {
-			newIndex = showcase.length - 1;
-		}
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= showcase.length) {
+      newIndex = showcase.length - 1;
+    }
 
-		setActiveIndex(newIndex);
-	};
+    setActiveIndex(newIndex);
+  };
 
-	return (
-		<div className="showcase-carousel">
-			<div className="inner" style={{ transform: `translate(-${activeIndex * 100}%)` }}>
-				{showcase.map((item) => {
-					return <ShowcaseItem key={item.id} item={item} />;
-				})}
-			</div>
-			<div className="carousel-buttons">
-				<button
-					className="button-arrow"
-					onClick={() => {
-						updateIndex(activeIndex - 1);
-					}}
-				>
-					Tillbaka
-				</button>
-				<div className="indicators">
-					{showcase.map((item, index) => {
-						return (
-							<button key={index} className="indicator-button">
-								*
-							</button>
-						);
-					})}
-				</div>
-				<button
-					className="button-arrow"
-					onClick={() => {
-						updateIndex();
-					}}
-				>
-					Framåt
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="showcase-carousel">
+      <div
+        className="inner"
+        style={{ transform: `translate(-${activeIndex * 100}%)` }}
+      >
+        {showcase.map((item) => {
+          return <ShowcaseItem key={item.id} item={item} />;
+        })}
+      </div>
+      <div className="showcase-carousel-buttons">
+        <button
+          className="button-arrow"
+          onClick={() => {
+            updateIndex(activeIndex - 1);
+          }}
+        >
+          <i class="fa-light fa-angle-left"></i>
+        </button>
+        <div className="indicators">
+          {showcase.map((item, index) => {
+            return (
+              <button key={index} className="indicator-button">
+                <i class="fa-solid fa-period"></i>
+              </button>
+            );
+          })}
+        </div>
+        <button
+          className="button-arrow"
+          onClick={() => {
+            updateIndex();
+          }}
+        >
+          <i class="fa-light fa-angle-right"></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Showcase;
