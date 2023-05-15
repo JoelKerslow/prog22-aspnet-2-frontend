851d6e612c7c18be46328caee586850ac2627d16
robin <robin.andersson@me.com>
2023-05-15 12:57:37 +0200

    Add import and route for ProductReviewForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Welcome from './components/views/Welcome'
-import Onboarding from './components/views/Onboarding'
-import VerificationCode from './components/views/VerificationCode'
-import SearchProducts from './components/views/SearchProducts'
-import ProductSearchResults from './components/views/ProductSearchResults'
-import ProductContextProvider from './contexts/ProductContext'
-import CategoryResults from './components/views/CategoryResults'
-import Signin from './components/partials/Signin'
-import Signup from './components/partials/Signup'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./components/views/Welcome";
+import Onboarding from "./components/views/Onboarding";
+import VerificationCode from "./components/views/VerificationCode";
+import SearchProducts from "./components/views/SearchProducts";
+import ProductSearchResults from "./components/views/ProductSearchResults";
+import ProductContextProvider from "./contexts/ProductContext";
+import CategoryResults from "./components/views/CategoryResults";
+import Signin from "./components/partials/Signin";
+import Signup from "./components/partials/Signup";
+import ProductReviewForm from "./components/views/ProductReviewForm";
 
 function App() {
   return (
@@ -16,25 +17,26 @@ function App() {
       <ProductContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<Welcome />} />
-            <Route path="/Onboarding" element={<Onboarding />} />
-            <Route path="/Verification" element={<VerificationCode />} />
-            <Route path="/SearchProducts" element={<SearchProducts />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/Signin" element={<Signin />} />
+            <Route exact path='/' element={<Welcome />} />
+            <Route path='/Onboarding' element={<Onboarding />} />
+            <Route path='/Verification' element={<VerificationCode />} />
+            <Route path='/SearchProducts' element={<SearchProducts />} />
+            <Route path='/Signup' element={<Signup />} />
+            <Route path='/Signin' element={<Signin />} />
+            <Route path='/ProductReview' element={<ProductReviewForm />} />
             <Route
-              path="/ProductSearchResults/:searchVal"
+              path='/ProductSearchResults/:searchVal'
               element={<ProductSearchResults />}
             />
             <Route
-              path="/CategoryResults/:departmentId/:categoryId"
+              path='/CategoryResults/:departmentId/:categoryId'
               element={<CategoryResults />}
             />
           </Routes>
         </BrowserRouter>
       </ProductContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
