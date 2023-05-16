9f93232a20fc2dbeffc7097b1d684a1db1265483
robin <robin.andersson@me.com>
2023-05-16 15:12:41 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import routingConfig from "./routing.json"
+import React from "react"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Welcome from "./components/views/Welcome"
@@ -13,30 +15,56 @@ import ProfilePage from "./components/views/ProfilePage"
 import ProductReviewForm from "./components/views/ProductReviewForm"
 
 function App() {
+  const { routes, fallback } = routingConfig
+  const routeElements = routes.map((route) => (
+    <Route
+      key={route.path}
+      path={route.path}
+      element={React.createElement(
+        require(`./components/views/${route.element}`).default
+      )}
+    />
+  ))
+
+  const fallbackElement = React.createElement(
+    require(`./components/views/${fallback.element}`).default
+  )
+
   return (
-    <>
-      <AuthorizationContextProvider>
-        <ProductContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route exact path='/' element={<Welcome />} />
-              <Route path='*' element={<Welcome />} />
-              <Route path='/Onboarding' element={<Onboarding />} />
-              <Route path='/Verification' element={<VerificationCode />} />
-              <Route path='/SearchProducts' element={<SearchProducts />} />
-              <Route path='/Signup' element={<Signup />} />
-              <Route path='/Signin' element={<Signin />} />
-              <Route path='/Profile' element={<ProfilePage />} />
-              <Route
-                path='/ProductReview/:productId'
-                element={<ProductReviewForm />}
-              />
-              <Route path='/Products/:type/:value' element={<Products />} />
-            </Routes>
-          </BrowserRouter>
-        </ProductContextProvider>
-      </AuthorizationContextProvider>
-    </>
+    <AuthorizationContextProvider>
+      <ProductContextProvider>
+        <BrowserRouter>
+          <Routes>
+            {routeElements}
+            <Route path='*' element={fallbackElement} />
+          </Routes>
+        </BrowserRouter>
+      </ProductContextProvider>
+    </AuthorizationContextProvider>
+
+    // <>
+    //   <AuthorizationContextProvider>
+    //     <ProductContextProvider>
+    //       <BrowserRouter>
+    //         <Routes>
+    //           <Route exact path='/' element={<Welcome />} />
+    //           <Route path='*' element={<Welcome />} />
+    //           <Route path='/Onboarding' element={<Onboarding />} />
+    //           <Route path='/Verification' element={<VerificationCode />} />
+    //           <Route path='/SearchProducts' element={<SearchProducts />} />
+    //           <Route path='/Signup' element={<Signup />} />
+    //           <Route path='/Signin' element={<Signin />} />
+    //           <Route path='/Profile' element={<ProfilePage />} />
+    //           <Route
+    //             path='/ProductReview/:productId'
+    //             element={<ProductReviewForm />}
+    //           />
+    //           <Route path='/Products/:type/:value' element={<Products />} />
+    //         </Routes>
+    //       </BrowserRouter>
+    //     </ProductContextProvider>
+    //   </AuthorizationContextProvider>
+    // </>
   )
 }
 
