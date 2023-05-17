bf97c34bd41f02fb3676b5a17065a98dc8399640
Moonlit <100379902+MrMoonlit@users.noreply.github.com>
2023-05-17 11:37:14 +0200

    bla

--- src/components/partials/UserProductReview.jsx
+++ src/components/partials/UserProductReview.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import StarRating from './StarRating'
 
 const UserProductReview = () => {
   return (
-    <div>
-      User review card 
+    <div className='reviewCard'>
+      <div className='cardHeader'>
+        <div className='profileImg'><img src='https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png'></img></div>
+        <div className='nameAndDate'>
+          <div className='profileName'>Anders Jansson</div>
+          <div className='reviewDate'>23 jan 2022</div>
+        </div>
+        <div className='reviewStarRating'><StarRating /></div>
+      </div>
+
+      <div className='reviewText'>Consequat ut ea dolor laborom tempor Lorem culpa. Commodo veniam sint est mollit prodient commodo</div>
     </div>
   )
 }
