b86f9ade66db63d8d36c5282a231b3c1e8382f17
Moonlit <100379902+MrMoonlit@users.noreply.github.com>
2023-05-19 10:10:09 +0200

    init

--- src/components/views/ProductReview.jsx
+++ src/components/views/ProductReview.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import UserProductReview from '../partials/UserProductReview'
 import ProductReviewSiteHead from '../partials/ProductReviewSiteHead'
-
+import { ProductReviewsAsync } from "../../services/ReviewServices";
+import { useParams } from 'react-router'
 
 const ProductReview = () => {
+  const [reviewList, setReviewList] = useState([]);
+  const { productId } = useParams();
+
+  useEffect(()=>{
+    const GetReviewsAsync = async () => {
+      ProductReviewsAsync(1).then((data) => setReviewList(data));
+      console.log({reviewList})
+    };
+    GetReviewsAsync();
+  }, []);
+
   return (
     <div>
         <ProductReviewSiteHead />
--- src/services/ReviewServices.js
+++ src/services/ReviewServices.js
@@ -1,15 +1,17 @@
-const ProductReviewsAsync = async (productId) =>{
-    const url = "https://aspnet2-grupp1-backend.azurewebsites.net/api/ProductReviews?productId=" + {productId};
-    const apiKey = "f77ca749-67f4-4c22-9039-137272442ea0";
+const ProductReviewsAsync = async (productId) => {
+	const url =
+		"https://aspnet2-grupp1-backend.azurewebsites.net/api/ProductReviews?productId=" +
+		{ productId };
+	const apiKey = "f77ca749-67f4-4c22-9039-137272442ea0";
 
-    const result = await fetch(url, {
-        headers: {
-          "API-KEY" : apiKey,
-        }
-      })
-    
-      const data = await result.json();
-      return data;
-}
+	const result = await fetch(url, {
+		headers: {
+			"API-KEY": apiKey,
+		},
+	});
 
-export {ProductReviewsAsync}
+	const data = await result.json();
+	return data;
+};
+
+export { ProductReviewsAsync };
