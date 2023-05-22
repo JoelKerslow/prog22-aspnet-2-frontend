56c7344ef2fed649cc4b5098383a5260ba0269e9
Robin Andersson <robin.andersson@me.com>
2023-05-22 20:03:30 +0200

    Move create review to ReviewServices

--- src/services/ReviewServices.js
+++ src/services/ReviewServices.js
@@ -1,9 +1,14 @@
-const ProductReviewsAsync = async (productId) => {
-	const url =
-		"https://aspnet2-grupp1-backend.azurewebsites.net/api/ProductReviews?productId=";
-	const apiKey = "f77ca749-67f4-4c22-9039-137272442ea0";
+import Cookies from "js-cookie"
 
-	const result = await fetch(url + productId, {
+const productReviewsUrl = "https://aspnet2-grupp1-backend.azurewebsites.net/api/ProductReviews"
+const apiKey = "f77ca749-67f4-4c22-9039-137272442ea0"
+
+const formatCommentOrNull = (comment) => comment ? comment.replace(/\s+/g, " ").trim() : null
+
+const ProductReviewsAsync = async (productId) => {
+	const url = `${productReviewsUrl}?productId=${productId}`
+
+	const result = await fetch(url, {
 		headers: {
 			"API-KEY": apiKey,
 		},
@@ -13,4 +18,34 @@ const ProductReviewsAsync = async (productId) => {
 	return data;
 };
 
-export { ProductReviewsAsync };
+const createProductReviewAsync = async (reviewData) => {
+	const requestData = {
+		...reviewData,
+		comment: formatCommentOrNull(reviewData.comment),
+	}
+
+	const result = await fetch(productReviewsUrl, {
+		method: "POST",
+		headers: {
+			"API-KEY": apiKey,
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + Cookies.get("maneroToken"),
+		},
+		body: JSON.stringify(requestData),
+	})
+		.then((res) => {
+			if (res.ok) {
+				return true
+			}
+
+			return false
+		})
+		.catch((err) => {
+			console.error(err.message)
+			return false
+		})
+
+	return result
+}
+
+export { ProductReviewsAsync, createProductReviewAsync };
