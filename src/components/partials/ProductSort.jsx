9a810a1db11de01576a0d443e745dd02dc8bfd1c
Werre6 <oscarwerre@gmail.com>
2023-05-24 14:50:56 +0200

    Sorting logic changed

--- src/components/partials/ProductSort.jsx
+++ src/components/partials/ProductSort.jsx
@@ -8,7 +8,7 @@ const ProductSort = ({
   setProducts,
   setShowSorting,
   activeSorting,
-  setActiveSorting
+  setActiveSorting,
 }) => {
   const sortings = [
     'Newest',
@@ -21,41 +21,40 @@ const ProductSort = ({
 
   const handleSorting = (sorting) => {
     let sortedProducts = []
-    if (!isFiltered) {
-      sortedProducts = [...products].sort((a, b) => {
-        if (sorting === 'Newest') {
-          const date1 = new Date(a.createdAt)
-          const date2 = new Date(b.createdAt)
-          return date2 - date1
+    sortedProducts = [...products].sort((a, b) => {
+      if (sorting === 'Newest') {
+        const date1 = new Date(a.createdAt)
+        const date2 = new Date(b.createdAt)
+        return date2 - date1
+      }
+      if (sorting === 'Oldest') {
+        const date1 = new Date(a.createdAt)
+        const date2 = new Date(b.createdAt)
+        return date1 - date2
+      }
+      if (sorting === 'A-Z') {
+        const name1 = a.name.toLowerCase()
+        const name2 = b.name.toLowerCase()
+        if (name1 < name2) {
+          return -1
         }
-        if (sorting === 'Oldest') {
-          const date1 = new Date(a.createdAt)
-          const date2 = new Date(b.createdAt)
-          return date1 - date2
+        if (name1 > name2) {
+          return 1
         }
-        if (sorting === 'A-Z') {
-          const name1 = a.name.toLowerCase()
-          const name2 = b.name.toLowerCase()
-          if (name1 < name2) {
-            return -1
-          }
-          if (name1 > name2) {
-            return 1
-          }
-          return 0
-        }
-        if (sorting === 'Highest Price') {
-          return b.price - a.price
-        }
-        if (sorting === 'Lowest Price') {
-          return a.price - b.price
-        }
-        if (sorting === 'Rating') {
-          return b.reviewAverage - a.reviewAverage
-        }
-      })
-      setProducts(sortedProducts)
-    } else {
+        return 0
+      }
+      if (sorting === 'Highest Price') {
+        return b.price - a.price
+      }
+      if (sorting === 'Lowest Price') {
+        return a.price - b.price
+      }
+      if (sorting === 'Rating') {
+        return b.reviewAverage - a.reviewAverage
+      }
+    })
+    setProducts(sortedProducts)
+    if (isFiltered) {
       sortedProducts = [...filteredList].sort((a, b) => {
         if (sorting === 'Newest') {
           const date1 = new Date(a.createdAt)
@@ -94,10 +93,10 @@ const ProductSort = ({
   }
 
   const sortingList = sortings.map((s) => {
-    const isActive = activeSorting === s;
+    const isActive = activeSorting === s
     return (
       <li
-        className={isActive ? "active-sorting" : ''}
+        className={isActive ? 'active-sorting' : ''}
         key={s}
         onClick={() => {
           handleSorting(s)
