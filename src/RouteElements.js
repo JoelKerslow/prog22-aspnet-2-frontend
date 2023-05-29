be052ea5ce7c69a98e40c577f112812bcfe046f3
robin <robin.andersson@me.com>
2023-05-29 14:02:11 +0200

    Update AddressForm route and add Addresses route

--- src/RouteElements.js
+++ src/RouteElements.js
@@ -16,6 +16,7 @@ import ProductReview from './components/views/ProductReview'
 import ForgotPassword from './components/views/ForgotPassword'
 import PasswordResetConfirmation from "./components/views/PasswordResetConfirmation"
 import ResetPassword from "./components/views/ResetPassword"
+import UserAddresses from "./components/views/UserAddresses"
 import AddressForm from "./components/views/AddressForm"
 import OrderHistory from "./components/views/OrderHistory"
 import MyPromocodes from "./components/views/MyPromocodes"
@@ -43,7 +44,8 @@ const RouteElements = () => {
       <Route path='/OrderReview/:orderId' element={<OrderReviewForm />} />
       <Route path='/Products/:type/:value' element={<Products />} />
       <Route path='/Profile/PaymentMethods' element={<PaymentMethod />} />
-      <Route path='/Profile/AddAddress' element={<AddressForm />} />
+      <Route path='/Profile/Addresses' element={<UserAddresses />} />
+      <Route path='/Profile/Addresses/Manage' element={<AddressForm />} />
       <Route path='/MyPromocodes' element={<MyPromocodes />} />
       <Route
         path='/ProductSubmitedReviews/:productId'
