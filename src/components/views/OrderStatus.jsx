e0873c891f5b70222a3c5150de031f59181a437e
aledo <aledo@DESKTOP-81I1KRD>
2023-05-31 14:38:48 +0200

    Done with Order tracking, implementation ready for different order statuses

--- src/components/views/OrderStatus.jsx
+++ src/components/views/OrderStatus.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from '../partials/Header'
 import CircleWithIcon from '../partials/generalPartials/CircleWithIcon'
 import VerticalBar from '../partials/generalPartials/VerticalBar'
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 
-const OrderStatus = ({ orderNumber, orderStatus }) => {
+const OrderStatus = () => {
   const navigate = useNavigate();
 
-//   useEffect(() => {
-//     if(orderNumber === null || orderNumber === undefined){
-//         navigate("/OrderHistory");
-//       }
-//   })
+  useEffect(() => {
+    if(currentOrder === null || currentOrder === undefined){
+        navigate("/OrderHistory")
+    }
+  });
+
+  const { currentOrder } = useContext(UserContext);
+  const newDate = new Date(currentOrder.orderDate);
+  const fixedOrderDate = `${newDate.getDate()} ${newDate.toLocaleString('default', { month: 'long' })} ${newDate.getFullYear()}`;
 
   return (
     <>
@@ -23,42 +28,68 @@ const OrderStatus = ({ orderNumber, orderStatus }) => {
             <VerticalBar />
             <div className='orderText'>
                 <h2>Your order:</h2>
-                <p>#{orderNumber}53252352</p>
+                <p>#{currentOrder.id}</p>
             </div>
-            <div className='container mt-5 mx-2'>
-                <div className='row'>
-                    <div className='col-3 tracking-icons mt-3'>
+            <div className='container-fluid mt-5'>
+                <div className='row big-row'>
+                    <div className='first-row col'>
                         <i className="fa-regular fa-circle-dot fa-2xl"></i>
                         <VerticalBar />
-                        <i className="fa-regular fa-circle-dot fa-2xl"></i>
-                        <VerticalBar />
-                        <i className="fa-regular fa-circle-dot fa-2xl"></i>
-                        <VerticalBar />
-                        <i className="fa-regular fa-circle-dot fa-2xl"></i>
-                        <VerticalBar />
-                        <i className="fa-regular fa-circle-dot fa-2xl"></i>
                     </div>
-                    <div className='text-order-track col-9'>
-                        <div className='text-bubble1'>
-                            <h6 className='text-order-status'>Order created</h6>
-                            <p className='under-text-order-status'>date and stuff</p>
-                        </div>
-                        <div className='text-bubble2'>
-                            <h6 className='text-order-status'>Order created</h6>
-                            <p className='under-text-order-status'>date and stuff</p>
-                        </div>
-                        <div className='text-bubble3'>
-                            <h6 className='text-order-status'>Order created</h6>
-                            <p className='under-text-order-status'>date and stuff</p>
-                        </div>
-                        <div className='text-bubble4'>
-                            <h6 className='text-order-status'>Order created</h6>
-                            <p className='under-text-order-status'>date and stuff</p>
-                        </div>
-                        <div className='text-bubble5'>
-                            <h6 className='text-order-status'>Order created</h6>
-                            <p className='under-text-order-status'>date and stuff</p>
-                        </div>
+                    <div className='text-bubble col'>
+                        <h6 className='text-order-status'>Order created</h6>
+                        { fixedOrderDate ? <p className='under-text-order-status'>{fixedOrderDate}</p> : <p className='under-text-order-status'>Date Unavailable</p>}
+                    </div>
+                </div>
+                <div className='row big-row'>
+                    <div className='first-row col'>
+                        { currentOrder.status === "Pending"
+                        ? <i className="fa-regular fa-circle-dot fa-2xl"></i>
+                        : <i class="fa-regular fa-circle fa-2xl"></i>
+                        }
+                        <VerticalBar />
+                    </div>
+                    <div className='text-bubble col'>
+                        <h6 className='text-order-status'>Order confirmed</h6>
+                        <p className='under-text-order-status'>date and stuff</p>
+                    </div>
+                </div>
+                <div className='row big-row'>
+                    <div className='first-row col'>
+                        { currentOrder.status === "Shipping"
+                        ? <i className="fa-regular fa-circle-dot fa-2xl"></i>
+                        : <i class="fa-regular fa-circle fa-2xl"></i>
+                        }
+                        <VerticalBar />
+                    </div>
+                    <div className='text-bubble col'>
+                        <h6 className='text-order-status'>Order shipping</h6>
+                        <p className='under-text-order-status'>date and stuff</p>
+                    </div>
+                </div>
+                <div className='row big-row'>
+                    <div className='first-row col'>
+                        { currentOrder.status === "Delivering"
+                        ? <i className="fa-regular fa-circle-dot fa-2xl"></i>
+                        : <i class="fa-regular fa-circle fa-2xl"></i>
+                        }
+                        <VerticalBar />
+                    </div>
+                    <div className='text-bubble col'>
+                        <h6 className='text-order-status'>Courier delivering</h6>
+                        <p className='under-text-order-status'>date and stuff</p>
+                    </div>
+                </div>
+                <div className='row big-row'>
+                    <div className='first-row col'>
+                        { currentOrder.status === "Receiving"
+                        ? <i className="fa-regular fa-circle-dot fa-2xl"></i>
+                        : <i class="fa-regular fa-circle fa-2xl"></i>
+                        }
+                    </div>
+                    <div className='text-bubble col'>
+                        <h6 className='text-order-status'>Receiving</h6>
+                        <p className='under-text-order-status'>date and stuff</p>
                     </div>
                 </div>
             </div>
