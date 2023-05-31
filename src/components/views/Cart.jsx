46c88ead215a1c5d327aaa5a4a4ab860e9618828
Werre6 <oscarwerre@gmail.com>
2023-05-31 10:32:50 +0200

    Cart NavBar Location

--- src/components/views/Cart.jsx
+++ src/components/views/Cart.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import BackArrow from "../partials/generalPartials/BackArrow";
-import Navbar from "../partials/Navbar";
-import { CartContext } from "../../contexts/CartContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
-import {placeholderImage} from '../../contexts/ProductContext'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import BackArrow from '../partials/generalPartials/BackArrow'
+import Navbar from '../partials/Navbar'
+import { CartContext } from '../../contexts/CartContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { placeholderImage } from '../../contexts/ProductContext'
 
 const Cart = () => {
   const {
@@ -16,40 +16,40 @@ const Cart = () => {
     updateQuantity,
     applyPromoCode,
     addCartItem,
-  } = useContext(CartContext);
+  } = useContext(CartContext)
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleIncrement = (productId, quantity) => {
-    const newQuantity = quantity + 1;
-    updateQuantity(productId, newQuantity);
-  };
+    const newQuantity = quantity + 1
+    updateQuantity(productId, newQuantity)
+  }
 
   const handleDecrement = (productId, quantity) => {
     if (quantity > 1) {
-      const newQuantity = quantity - 1;
-      updateQuantity(productId, newQuantity);
+      const newQuantity = quantity - 1
+      updateQuantity(productId, newQuantity)
     }
-  };
+  }
 
   const handleDeleteItem = (productId) => {
-    deleteItem(productId);
-  };
+    deleteItem(productId)
+  }
 
   const handleAddToCart = (productId, quantity) => {
     const updatedQuantity =
-      quantity === 0 ? quantity + 1 : quantity; /*  For adding to cart */
-    addCartItem(productId, updatedQuantity);
-  };
+      quantity === 0 ? quantity + 1 : quantity /*  For adding to cart */
+    addCartItem(productId, updatedQuantity)
+  }
 
   const handlePromoCode = (e) => {
-    setCode(e.target.value);
-    applyPromoCode(code);
-  };
+    setCode(e.target.value)
+    applyPromoCode(code)
+  }
 
   const handleGoBack = () => {
-    navigate(-1);
-  };
+    navigate(-1)
+  }
 
   return (
     <>
@@ -68,7 +68,11 @@ const Cart = () => {
               <div className="cart-image-section">
                 <img
                   className="cart-image"
-                  src={item.product.imageUrl !== null ? item.product.imageUrl : placeholderImage}
+                  src={
+                    item.product.imageUrl !== null
+                      ? item.product.imageUrl
+                      : placeholderImage
+                  }
                   alt=""
                 />
                 {cart.promoCode && <div className="sale-badge">SALE</div>}
@@ -78,7 +82,7 @@ const Cart = () => {
                 <div className="item-price">
                   <p>${item.product.price}</p>
                   {cart.promoCode && (
-                    <p style={{ color: "red" }}>
+                    <p style={{ color: 'red' }}>
                       $
                       {(item.product.price *
                         item.quantity *
@@ -115,7 +119,7 @@ const Cart = () => {
               >
                 <FontAwesomeIcon
                   icon={faTrash}
-                  style={{ color: "lightgray" }}
+                  style={{ color: 'lightgray' }}
                 />
               </div>
             </div>
@@ -153,11 +157,11 @@ const Cart = () => {
           <table className="table">
             <tbody>
               <tr>
-                <td className="title" style={{ fontWeight: "bold" }}>
+                <td className="title" style={{ fontWeight: 'bold' }}>
                   Subtotal
                 </td>
-                <td className="value" style={{ fontWeight: "bold" }}>
-                 ${cart.totalAmountWithoutDiscount}
+                <td className="value" style={{ fontWeight: 'bold' }}>
+                  ${cart.totalAmountWithoutDiscount}
                 </td>
               </tr>
               <tr>
@@ -166,15 +170,15 @@ const Cart = () => {
               </tr>
               <tr>
                 <td className="title">Delivery</td>
-                <td className="value" style={{ color: "green" }}>
+                <td className="value" style={{ color: 'green' }}>
                   Free
                 </td>
               </tr>
               <tr>
-                <td className="title" style={{ fontWeight: "bold" }}>
+                <td className="title" style={{ fontWeight: 'bold' }}>
                   Total
                 </td>
-                <td className="value" style={{ fontWeight: "bold" }}>
+                <td className="value" style={{ fontWeight: 'bold' }}>
                   ${cart.totalAmountWithDiscount}
                 </td>
               </tr>
@@ -184,14 +188,14 @@ const Cart = () => {
         <div className="button-section">
           <button className="BigBlackButton">PROCEED TO CHECKOUT</button>
         </div>
-        <div className="profile-navbar">
-          <Navbar />
-        </div>
-   {/*      {<button onClick={() => handleAddToCart(7, 4)}>AddProduct</button>}{" "} */}
+        {/*      {<button onClick={() => handleAddToCart(7, 4)}>AddProduct</button>}{" "} */}
         {/*  Test  for adding to cart */}
       </div>
+      <div className="profile-navbar">
+        <Navbar />
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
