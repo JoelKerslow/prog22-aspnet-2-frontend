e47b4359fdfde8249dfa579e30b4ac1c8e20c15c
Werre6 <oscarwerre@gmail.com>
2023-06-01 10:44:47 +0200

    Call ProductSearch on Enter press

--- src/components/partials/SearchField.jsx
+++ src/components/partials/SearchField.jsx
@@ -6,21 +6,27 @@ const SearchField = () => {
   const navigate = useNavigate()
   const searchVal = useRef()
 
+  const handleSearch = () => {
+    navigate(
+      searchVal.current.value === ''
+        ? '/SearchProducts'
+        : `/Products/search/${searchVal.current.value}`
+    )
+    searchVal.current.value = ''
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') handleSearch()
+  }
+
   return (
     <div className="search-field">
       <label>
         <button
           className="fa-thin fa-magnifying-glass fa-xs"
-          onClick={() => {
-            navigate(
-              searchVal.current.value === ''
-                ? '/SearchProducts'
-                : `/Products/search/${searchVal.current.value}`
-            )
-            searchVal.current.value = ''
-          }}
+          onClick={handleSearch}
         ></button>
-        <input type="text" placeholder="Search for products" ref={searchVal} />
+        <input type="text" placeholder="Search for products" ref={searchVal} onKeyDown={handleKeyDown}/>
       </label>
     </div>
   )
